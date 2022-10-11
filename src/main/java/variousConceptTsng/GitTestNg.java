f6557fa2f91faf20efd252fd43726f7263fc78e0
varalakshmip <danduvaralakshmi@gmail.com>
2022-10-11 00:13:31 -0500

    Test Class

--- src/main/java/variousConceptTsng/GitTestNg.java
+++ src/main/java/variousConceptTsng/GitTestNg.java
@@ -1,9 +1,11 @@
 package variousConceptTsng;
 
+
+
 public class GitTestNg {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("First TestNg");
 
 	}
 
